3bbe93b90288b79aeb73b8c4679f5717be51cfdc
JimakPasha <Jimak@tut.by>
2021-06-13 22:21:36 +0300

    add 5 bg in cards project

--- cards/script.js
+++ cards/script.js
@@ -2,13 +2,15 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slide'),
+bg = document.querySelector('.body');
 
 
 slides.forEach((item) => {
 	item.addEventListener('click', () => {
-	clearActiveClasses();
-
+		clearActiveClasses();
+		const element = item.getAttribute('data-element');
+		bg.style.backgroundImage = `url(images/${element + '-bg'}.jpg)`;
 		item.classList.add('active');
 	});
 });
