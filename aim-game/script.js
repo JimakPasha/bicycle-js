2154e3f012877c1087fb349507475a20764bab67
JimakPasha <Jimak@tut.by>
2021-06-20 16:11:54 +0300

    feat: add btn repeat

--- aim-game/script.js
+++ aim-game/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const startBtn = document.querySelector('#start'),
-	repeatBtn = document.querySelector('#repeat'),
 	screens = document.querySelectorAll('.screen'),
 	timeList = document.querySelector('.time__list'),
 	dashboard = document.querySelector('.game__dashboard'),
@@ -12,6 +11,7 @@ const startBtn = document.querySelector('#start'),
 
 let time = 0;
 let score = 0;
+let idInterval;
 
 
 startBtn.addEventListener('click', () => {
@@ -36,7 +36,7 @@ board.addEventListener('click', (e) => {
 });
 
 function startGame() {
-	setInterval(decreaseTime ,1000);
+	idInterval = setInterval(decreaseTime ,1000);
 	createRandomCircle();
 	setTime(time);
 }
@@ -72,7 +72,6 @@ function createRandomCircle() {
 	setColor(circle);
 
 	board.append(circle);
-
 }
 
 function getRandomNumber(min, max) {
@@ -92,5 +91,29 @@ function setColor(element) {
 
 function finishGame() {
 	dashboard.classList.add('hide');
-	board.innerHTML = `<h3 class="score__title">Счёт: <span class="score__title-num">${score}</span></h3>`;
+	board.innerHTML = `
+	<div class="score">
+		<h3 class="score__title">
+			Счёт: <span class="score__title-num">${score}</span>
+		</h3>
+		<button class="score__btn" id="repeat">
+			Сыграть ещё
+		</button>
+	</div>
+	`;
+	clearInterval(idInterval);
+	const repeatBtn = document.querySelector('#repeat');
+	repeatBtn.addEventListener('click', () => {
+		screens.forEach((item) => {
+			if (item.classList.contains('up')) {
+				item.classList.remove('up');
+			}
+		});
+		screens[0].classList.add('up');
+		dashboard.classList.remove('hide');
+		board.innerHTML = '';
+		score = 0;
+		scoreDashboard.innerHTML = '0';
+	});
+
 }
